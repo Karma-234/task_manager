536825d1ffee276cc5b168c5801f8b48c0b9622a
Karma-234 <busari1kamal@gmail.com>
2023-09-21 12:55:20 -0700

    Set up local storage

--- lib/core/datasource/local_storage.dart
+++ lib/core/datasource/local_storage.dart
@@ -1,1 +1,15 @@
-class AppLocalStorage {}
+import 'package:hive_flutter/hive_flutter.dart';
+
+class AppLocalStorage {
+  static initFlutter() async {
+    await Hive.initFlutter();
+    Hive.openBox('task.manger');
+  }
+
+  Box? box;
+  void addUser(String user) {
+    box?.put('user', user);
+  }
+
+  String? getUser() => box?.get('user');
+}
